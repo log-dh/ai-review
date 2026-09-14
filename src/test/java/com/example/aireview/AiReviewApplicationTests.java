96c546c0a35a837c013c0670225739eb9ab87ad0
log-dh <ekgml085212@gmail.com>
2026-09-14 13:54:16 +0900

    초기 설정

---
+++ src/test/java/com/example/aireview/AiReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aireview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
